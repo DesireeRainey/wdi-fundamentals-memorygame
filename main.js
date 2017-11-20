646fd9295083fc93c53f05a7199277d4f5d6879f
Desiree Rainey <Drainey21.DR@gmail.com>
2017-11-19 22:56:39 -0800

    created game logic

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ var cards = [
     suit: 'hearts',
     cardImage: 'images/queen-of-hearts.png'
   },
+
   {
     rank: 'queen',
     suit: 'diamonds',
     cardImage: 'images/queen-of-diamonds.png'
   },
+
   {
     rank: 'king',
     suit: 'hearts',
     cardImage: 'images/king-of-hearts.png'
   },
+
   {
     rank: 'king',
     suit: 'diamonds',
@@ -29,13 +32,24 @@ var checkForMatch = function () {
   }
 };
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
+  cardId = this.getAttribute('data-id');
+  console.log(cardId);
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
     checkForMatch();
+    cardsInPlay = [];
   }
 };
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+  var cardElement = document.createElement('img');
+  cardElement.setAttribute('src', 'images/back.png');
+  cardElement.setAttribute('data-id', i);
+  cardElement.addEventListener('click', flipCard);
+  document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+
+createBoard();
+
